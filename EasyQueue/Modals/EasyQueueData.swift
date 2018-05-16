c203d471b224984478d07c049faccf9f9c4bf64f
Nicholas <nicholas.k.santosa@student.uts.edu.au>
2018-05-17 02:25:36 +1000

    Add EasyQueueData.swift

---
+++ EasyQueue/Modals/EasyQueueData.swift
@@ -0,0 +1,25 @@
+//
+//  EasyQueueData.swift
+//  EasyQueue
+//
+//  Created by Nicholas on 17/5/18.
+//  Copyright © 2018 Nicholas. All rights reserved.
+//
+
+// put your constant here
+let AppName = "EasyQueue"
+
+
+// put your enum here
+//enum Example {
+//    case one
+//    case two
+//}
+
+
+// put your struct here
+//struct AnotherExample {
+//    var huh: String
+//    var well: String
+//}
+
