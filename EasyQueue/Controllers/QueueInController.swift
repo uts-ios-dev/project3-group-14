cf15a6451990092f185b97481e8c747d1668e3f4
Kunanon Pititheerachot <>
2018-05-24 00:42:50 +1000

    test

--- EasyQueue/Controllers/QueueInController.swift
+++ EasyQueue/Controllers/QueueInController.swift
@@ -12,30 +12,38 @@ class QueueInController: UIViewController {
     
     @IBOutlet weak var restNameLabel: UILabel!
     @IBOutlet weak var customerAmount: UITextField!
-    @IBOutlet weak var queueButton: UIButton!
+
     
     let testDB = EasyQueueDB()
     var restaurantName = "Restaurant"
     var restaurantID = 0
-    var custAmount = "0"
+    var custAmount = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         restNameLabel.text = restaurantName
         restaurantID = testDB.getRestaurantID(restaurantName: restaurantName)
-        custAmount = customerAmount.text!
-
-        
 //        customerAmount.text = String(restaurantID)
-//        if testDB.setQueue(uid: <#T##Int#>, rid: <#T##Int#>, num: <#T##Int#>, stat: ) == 0
     }
-    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
+//    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+//        if identifier == "theIdentifier" {
+//            custAmount = Int(customerAmount.text!)!
+//            testDB.setQueue(uid: 1, rid: restaurantID, num: custAmount, stat: 1)
+//
+//        }
+//        return true
+//    }
     
+    @IBAction func queueButton(_ sender: UIButton) {
+        custAmount = Int(customerAmount.text!)!
+        let result = testDB.setQueue(uid: 1, rid: restaurantID, num: custAmount, stat: 1)
+        performSegue(withIdentifier: "QueueSummary", sender: sender)
+    }
     
 //    override func prepare(for segue: UIStoryboardSegue, sender: Any?){
 //        if segue.identifier == "yourIdentifier" {
---
+++ EasyQueue/Controllers/QueueSummaryController.swift
@@ -0,0 +1,27 @@
+//
+//  QueueSummaryController.swift
+//  EasyQueue
+//
+//  Created by Kunanon Pititheerachot on 21/5/18.
+//  Copyright © 2018 Nicholas. All rights reserved.
+//
+
+import UIKit
+
+class QueueInController: UIViewController {
+
+    
+    let testDB = EasyQueueDB()
+    var restaurantName = "Restaurant"
+    var restaurantID = 0
+    var custAmount = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
